eb5aa6577eb1630c8414834cd90c68f2476c5a68
Grant Crofton <grantac@hotmail.com>
2017-04-24 15:57:49 +0100

    Started trial FP solution

---
+++ Solutions/Functional/Grant_FSharp/FunctionalBowling/FunctionalBowling/Bowling.fs
@@ -0,0 +1,12 @@
+﻿module Bowling
+
+let spare ball1 ball2 = ball1 + ball2 = 10
+let strike ball1 = ball1 = 10
+
+let rec calculate (score:int list) =
+    match score with
+    | [] -> 0
+    | ball1::rest when strike ball1 -> ball1 + (rest |> Seq.take 2 |> Seq.sum) + (calculate rest)
+    | ball1::ball2::rest when spare ball1 ball2 -> ball1 + ball2 + Seq.head rest + (calculate rest)
+    | ball1::ball2::rest -> ball1 + ball2 + (calculate rest)
+    | ball1::_ -> ball1
---
+++ Solutions/Functional/Grant_FSharp/FunctionalBowling/FunctionalBowling/Tests.fs
@@ -0,0 +1,27 @@
+﻿module Tests
+
+open NUnit.Framework
+open FsUnit
+
+open Bowling
+
+[<Test>]
+let ``simple scores should get the expected score`` () =
+    calculate [1] |> should equal 1
+    calculate [1;5;8;1] |> should equal 15
+
+[<Test>]
+let ``spare should include score of next ball`` () =
+    calculate [5;5;5] |> should equal 20
+    calculate [1;1;5;5;1;1;5;5;1] |> should equal 27
+
+[<Test>]
+let ``strike should include score of next two balls`` () =
+    calculate [10;5;4] |> should equal 28
+    calculate [10;1;1;10;1;1] |> should equal 28
+    calculate [10;10;10;1;1] |> should equal 65
+
+[<Test>]
+let ``balls after 10th frame should not be counted separately`` () =
+    calculate [1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;10;1;1] |> should equal 30
+
